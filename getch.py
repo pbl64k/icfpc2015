12f400cec8a7fce51e6a309552350da42ec54d3a
pbl64k <pbl64k@gmail.com>
2015-08-08 01:18:30 +0300

    getch()

---
+++ getch.py
@@ -0,0 +1,16 @@
+# StackOverflow. Non-core, *rolls eyes*.
+
+import sys
+import tty
+import termios
+
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(fd)
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
