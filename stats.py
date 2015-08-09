7a3048f0e4311e5f916b216204f8db5d10e44c51
pbl64k <pbl64k@gmail.com>
2015-08-09 16:57:11 +0300

    Tools.

--- stats.py
+++ stats.py
@@ -1,9 +1,16 @@
 import json
+import sys
+
+if len(sys.argv) > 1:
+    tag = sys.argv[1]
+else:
+    tag = 'NONSENSE'
 
 f = open('sols.txt', 'r')
 data = json.loads(f.read())
 
 ids = {}
+tag_ids = {}
 pending = []
 
 for x in data:
@@ -17,11 +24,18 @@ for x in data:
         ids[id][seed] = sc
     if sc is None:
         pending.append((id, seed, x['tag']))
+    if x['tag'][0:len(tag)] == tag:
+        if id not in tag_ids:
+            tag_ids[id] = {}
+        if seed not in tag_ids[id] or (sc is not None and ((tag_ids[id][seed] is None and sc > 0) or tag_ids[id][seed] < sc)):
+            tag_ids[id][seed] = sc
 
 for id in ids:
     print 'Problem', id
     for seed in ids[id]:
         print ids[id][seed],
+        if id in tag_ids and seed in tag_ids[id]:
+            print '(' + str(tag_ids[id][seed]) + ')',
     print
     print
 
